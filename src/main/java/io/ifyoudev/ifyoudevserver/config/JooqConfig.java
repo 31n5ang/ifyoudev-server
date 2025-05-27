81deeef341876e15f4f832dba5affd94c4bfa432
31n5ang <31n5ang@gmail.com>
2025-05-27 17:30:03 +0900

    feat: jOOQ 설정 컴포넌트 추가

---
+++ src/main/java/io/ifyoudev/ifyoudevserver/config/JooqConfig.java
@@ -0,0 +1,17 @@
+package io.ifyoudev.ifyoudevserver.config;
+
+import org.jooq.conf.ExecuteWithoutWhere;
+import org.springframework.boot.autoconfigure.jooq.DefaultConfigurationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JooqConfig {
+
+    @Bean
+    public DefaultConfigurationCustomizer jooqDefaultConfigurationCustomizer() {
+        return configuration -> configuration.settings()
+                .withExecuteUpdateWithoutWhere(ExecuteWithoutWhere.THROW)
+                .withExecuteDeleteWithoutWhere(ExecuteWithoutWhere.THROW);
+    }
+}
