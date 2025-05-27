019492d41c5dcaddc164b6e805177e1064515ba2
31n5ang <31n5ang@gmail.com>
2025-05-27 17:28:47 +0900

    init: 프로젝트 생성

---
+++ src/main/java/io/ifyoudev/ifyoudevserver/IfyoudevApplication.java
@@ -0,0 +1,13 @@
+package io.ifyoudev.ifyoudevserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfyoudevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IfyoudevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/ifyoudev/ifyoudevserver/IfyoudevApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ifyoudev.ifyoudevserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfyoudevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
