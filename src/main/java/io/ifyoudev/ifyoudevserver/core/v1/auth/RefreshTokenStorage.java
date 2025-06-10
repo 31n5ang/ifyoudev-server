2fd2d13ca76aaa9c7fe672f487fe63dfcfd21c4c
31n5ang <31n5ang@gmail.com>
2025-06-10 15:29:05 +0900

    feat: RefreshTokenStorage 인터페이스 추가

---
+++ src/main/java/io/ifyoudev/ifyoudevserver/core/v1/auth/RefreshTokenStorage.java
@@ -0,0 +1,32 @@
+package io.ifyoudev.ifyoudevserver.core.v1.auth;
+
+import java.util.Optional;
+
+public interface RefreshTokenStorage {
+
+    /**
+     * 사용자의 리프레시 토큰을 저장 또는 갱신합니다.
+     * 이미 토큰이 존재하는 사용자라면, 토큰을 갱신(교체)합니다. 갱신 후 이전 토큰은 저장되지 않으므로 주의해야합니다.
+     * @param userUuid 사용자의 UUID
+     * @param tokenValue 리프레시 토큰 값
+     * @param expirationMills 만료 시간(밀리초 단위)
+     * @author 31n5ang
+     */
+    void update(String userUuid, String tokenValue, long expirationMills);
+
+    /**
+     * 사용자의 리프레시 토큰을 조회합니다.
+     * @param userUuid 조회할 사용자의 UUID
+     * @return 저장된 리프레시 토큰 값
+     * @author 31n5ang
+     */
+    Optional<String> findByUserUuid(String userUuid);
+
+    /**
+     * 사용자의 리프레시 토큰을 삭제합니다.
+     * @param userUuid 삭제할 사용자의 UUID
+     * @return 삭제를 수행했다면 true, 삭제할 토큰이 존재하지 않으면 false
+     * @author 31n5ang
+     */
+    boolean deleteByUserUuid(String userUuid);
+}
