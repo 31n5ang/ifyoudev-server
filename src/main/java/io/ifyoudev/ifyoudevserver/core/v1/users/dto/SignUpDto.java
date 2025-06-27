8cf6e0187cfbc9ef49b32c7ca86afd138b2ca300
31n5ang <31n5ang@gmail.com>
2025-06-27 16:35:44 +0900

    chore: 주석 추가

--- src/main/java/io/ifyoudev/ifyoudevserver/core/v1/users/dto/SignUpDto.java
+++ src/main/java/io/ifyoudev/ifyoudevserver/core/v1/users/dto/SignUpDto.java
@@ -5,6 +5,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+/**
+ * Controller <-> Service DTO입니다.
+ */
 public record SignUpDto (
         @Email(message = "이메일 형식을 지켜주세요.")
         @NotBlank(message = "이메일은 필수 항목입니다.")
--- src/main/java/io/ifyoudev/ifyoudevserver/core/v1/users/dto/SignUpSuccessDto.java
+++ src/main/java/io/ifyoudev/ifyoudevserver/core/v1/users/dto/SignUpSuccessDto.java
@@ -1,5 +1,10 @@
 package io.ifyoudev.ifyoudevserver.core.v1.users.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+/**
+ * Controller <-> Service DTO입니다.
+ */
 public record SignUpSuccessDto(
         String userUuid
 ) {}
