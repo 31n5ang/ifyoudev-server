b154a8178c6193f12f37c2b163fb2e69881cee24
31n5ang <31n5ang@gmail.com>
2025-06-27 16:36:32 +0900

    feat: 레포지토리와 서비스 통신용 dto 추가

--- src/main/java/io/ifyoudev/ifyoudevserver/core/v1/users/UserService.java
+++ src/main/java/io/ifyoudev/ifyoudevserver/core/v1/users/UserService.java
@@ -3,15 +3,14 @@ package io.ifyoudev.ifyoudevserver.core.v1.users;
 import io.ifyoudev.ifyoudevserver.core.v1.roles.RoleType;
 import io.ifyoudev.ifyoudevserver.core.v1.users.dto.SignUpDto;
 import io.ifyoudev.ifyoudevserver.core.v1.users.dto.SignUpSuccessDto;
+import io.ifyoudev.ifyoudevserver.core.v1.users.dto.UserCreateDto;
 import io.ifyoudev.ifyoudevserver.core.v1.users.exception.business.UserSaveFailedException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.jooq.generated.tables.pojos.Users;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -25,19 +24,17 @@ public class UserService {
 
     @Transactional
     public SignUpSuccessDto signUp(SignUpDto signUpDto) {
-        Users users = new Users();
-        users.setUuid(UUID.randomUUID().toString());
-        users.setEmail(signUpDto.email());
-        users.setNickname(signUpDto.nickname());
-        users.setPassword(passwordEncoder.encode(signUpDto.password()));
-        users.setCreatedAt(LocalDateTime.now());
-        users.setLastModifiedAt(LocalDateTime.now());
+        UserCreateDto userCreateDto = new UserCreateDto();
+        userCreateDto.setUuid(UUID.randomUUID().toString());
+        userCreateDto.setEmail(signUpDto.email());
+        userCreateDto.setNickname(signUpDto.nickname());
+        userCreateDto.setPassword(passwordEncoder.encode(signUpDto.password()));
 
         try {
-            String userUuid = userRepository.saveWithReturningUuid(users);
-            userRepository.updateUserRoles(users.getUserId(), List.of(RoleType.USER));
-            log.info("사용자 '{}[{}]' signUp 성공", signUpDto.email(), userUuid);
-            return new SignUpSuccessDto(userUuid);
+            Long userId = userRepository.saveWithReturningId(userCreateDto);
+            userRepository.updateUserRoles(userId, List.of(RoleType.USER));
+            log.info("사용자 '{}[{}]' signUp 성공", signUpDto.email(), userId);
+            return new SignUpSuccessDto(userId, userCreateDto.getUuid());
         } catch (Exception ex) {
             log.error("사용자 '{}' signUp 실패", signUpDto.email(), ex);
             throw new UserSaveFailedException(signUpDto.email());
