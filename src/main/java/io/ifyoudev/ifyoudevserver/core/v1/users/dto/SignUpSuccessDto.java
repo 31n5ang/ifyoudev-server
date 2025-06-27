c75c27f94520132c07a01cf5c77dcd547e529b54
31n5ang <31n5ang@gmail.com>
2025-06-27 16:36:09 +0900

    chore: UserId를 회원가입 성공 dto에 추가

--- src/main/java/io/ifyoudev/ifyoudevserver/core/v1/users/dto/SignUpSuccessDto.java
+++ src/main/java/io/ifyoudev/ifyoudevserver/core/v1/users/dto/SignUpSuccessDto.java
@@ -6,5 +6,23 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
  * Controller <-> Service DTO입니다.
  */
 public record SignUpSuccessDto(
+        @JsonIgnore
+        Long userId,
         String userUuid
-) {}
+) {
+    public SignUpSuccessDto() {
+        this((String) null, null);
+    }
+
+    public SignUpSuccessDto(String userUuid) {
+        this(null, userUuid);
+    }
+
+    public SignUpSuccessDto(Long userId) {
+        this(userId, null);
+    }
+
+    public SignUpSuccessDto(String userUuid, Long userId) {
+        this(userId, userUuid);
+    }
+}
