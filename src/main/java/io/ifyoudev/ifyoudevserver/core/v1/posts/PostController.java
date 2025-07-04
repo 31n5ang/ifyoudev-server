88c45bbf6688442e5a11b188df23aa162ad3e1d1
31n5ang <31n5ang@gmail.com>
2025-07-04 19:52:01 +0900

    refactor: Secured에서 PreAuthorize로 변경

--- src/main/java/io/ifyoudev/ifyoudevserver/core/v1/posts/PostController.java
+++ src/main/java/io/ifyoudev/ifyoudevserver/core/v1/posts/PostController.java
@@ -7,7 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +24,7 @@ public class PostController {
         return null;
     }
 
-    @Secured("ROLE_USER")
+    @PreAuthorize("hasAnyRole('ROLE_USER')")
     @PostMapping
     public ResponseEntity<?> createPost(
             @AuthenticationPrincipal AuthUser authUser,
