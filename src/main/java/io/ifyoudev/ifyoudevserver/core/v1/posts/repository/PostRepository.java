6251c7f30f84be0a984d790cbaca2fe23caa4fd3
31n5ang <31n5ang@gmail.com>
2025-07-08 21:05:23 +0900

    feat: Post 관련 조회 로직 추가

--- src/main/java/io/ifyoudev/ifyoudevserver/core/v1/posts/repository/PostRepository.java
+++ src/main/java/io/ifyoudev/ifyoudevserver/core/v1/posts/repository/PostRepository.java
@@ -2,8 +2,10 @@ package io.ifyoudev.ifyoudevserver.core.v1.posts.repository;
 
 import io.ifyoudev.ifyoudevserver.core.v1.posts.dto.PostDetailDto;
 import io.ifyoudev.ifyoudevserver.core.v1.posts.dto.PostDto;
+import io.ifyoudev.ifyoudevserver.core.v1.posts.dto.PostTagDto;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository {
 
@@ -11,5 +13,11 @@ public interface PostRepository {
 
     Long savePostDetail(String userUuid, PostDetailDto postDetailDto);
 
-    void savePostTags(Long postId, List<Integer> postTagIds);
+    void savePostTags(Long postId, List<Long> postTagIds);
+
+    Optional<PostDto> findPostDtoByUuid(String postUuid);
+
+    Optional<PostDetailDto> findPostDetailDtoByUuid(String postUuid);
+
+    List<PostTagDto> findAllPostTagDto(String postUuid);
 }
