ac85fc7b14d92ff67e40b6ebc8e8bd7024d87684
31n5ang <31n5ang@gmail.com>
2025-07-17 04:11:50 +0900

    feat: Post View DTO 조회 추가

--- src/main/java/io/ifyoudev/ifyoudevserver/core/v1/posts/PostService.java
+++ src/main/java/io/ifyoudev/ifyoudevserver/core/v1/posts/PostService.java
@@ -2,6 +2,9 @@ package io.ifyoudev.ifyoudevserver.core.v1.posts;
 
 import io.ifyoudev.ifyoudevserver.core.v1.posts.dto.PostCreateDto;
 import io.ifyoudev.ifyoudevserver.core.v1.posts.dto.PostCreateSuccessDto;
+import io.ifyoudev.ifyoudevserver.core.v1.posts.dto.PostViewDto;
+import io.ifyoudev.ifyoudevserver.core.v1.posts.exception.PostNotFoundException;
+import io.ifyoudev.ifyoudevserver.core.v1.posts.repository.query.PostQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,9 +14,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostService {
 
     private final PostCreateManager postCreateManager;
+    private final PostQueryRepository postQueryRepository;
 
     @Transactional
     public PostCreateSuccessDto createPost(String userUuid, PostCreateDto postCreateDto) {
         return postCreateManager.init(userUuid, postCreateDto).create();
+    }
+
+    @Transactional(readOnly = true)
+    public PostViewDto findPostView(String postUuid) {
+        return postQueryRepository.findPostViewDto(postUuid)
+                .orElseThrow(() -> new PostNotFoundException("해당 Post[" + postUuid + "]의 PostView를 조회할 수 없습니다."));
     }
 }
