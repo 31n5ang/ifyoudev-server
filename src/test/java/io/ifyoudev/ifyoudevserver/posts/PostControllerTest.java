276589ecb801e399afbd1b1d6e0505402e6c611f
31n5ang <31n5ang@gmail.com>
2025-07-17 04:12:47 +0900

    test: Post View 조회 API 단위 테스트 추가

--- src/test/java/io/ifyoudev/ifyoudevserver/posts/PostControllerTest.java
+++ src/test/java/io/ifyoudev/ifyoudevserver/posts/PostControllerTest.java
@@ -6,8 +6,7 @@ import io.ifyoudev.ifyoudevserver.config.TestSecurityConfig;
 import io.ifyoudev.ifyoudevserver.core.v1.auth.AuthUser;
 import io.ifyoudev.ifyoudevserver.core.v1.posts.PostController;
 import io.ifyoudev.ifyoudevserver.core.v1.posts.PostService;
-import io.ifyoudev.ifyoudevserver.core.v1.posts.dto.PostCreateDto;
-import io.ifyoudev.ifyoudevserver.core.v1.posts.dto.PostCreateSuccessDto;
+import io.ifyoudev.ifyoudevserver.core.v1.posts.dto.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,10 +15,13 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.parameters.P;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
@@ -28,7 +30,10 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -51,6 +56,59 @@ public class PostControllerTest {
     void init() {
         // Jackson 라이브러리는 Java 8 에서 추가된 LocalDateTime을 직렬화, 역직렬화를 못하기 때문에 따로 등록
         objectMapper.registerModule(new JavaTimeModule());
+    }
+
+    @Test
+    @DisplayName("/{postUuid} - 포스트 조회 200 성공")
+    void getPostByPostUuidSuccessTest() throws Exception {
+        // given
+        String postUuid = UUID.randomUUID().toString();
+        AuthorDto authorDto = AuthorDto.builder()
+                .authorId(1L)
+                .authorUuid("테스트 작성자")
+                .build();
+
+        List<PostTagDto> postTagDtos = Arrays.asList(
+                PostTagDto.builder().tagId(1L).name("개발").build(),
+                PostTagDto.builder().tagId(2L).name("IT").build()
+        );
+
+        PostViewDto postViewDto = PostViewDto.builder()
+                .postId(1L)
+                .postUuid(postUuid)
+                .title("제목입니다.")
+                .content("이것은 포스트의 내용입니다.")
+                .summary("이것은 포스트의 요약입니다.")
+                .createdAt(LocalDateTime.now().minusDays(1))
+                .lastModifiedAt(LocalDateTime.now())
+                .authorDto(authorDto)
+                .deadline(LocalDateTime.now().plusDays(7))
+                .isOnline(true)
+                .isCompleted(false)
+                .sido("서울특별시")
+                .sigungu("금천구")
+                .postTagDtos(postTagDtos)
+                .build();
+
+        when(postService.findPostView(eq(postUuid))).thenReturn(postViewDto);
+
+        // when
+        ResultActions perform = mockMvc.perform(get("/api/v1/posts/view/{postUuid}", postUuid)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        perform.andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.postUuid").value(postViewDto.getPostUuid()))
+                .andExpect(jsonPath("$.title").value(postViewDto.getTitle()))
+                .andDo(print())
+                .andDo(document("posts/view/success",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("postUuid").description("조회할 Post의 UUID")
+                        )
+                ));
     }
 
     @Test
